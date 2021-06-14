a9f87c53aa575e6bade09716eb1c3d2c5d292ff9
vampirsoft <lordvampir@ymail.com>
2021-06-15 05:00:28 +0700

    Add crc-13

---
+++ sources/factories/chHash.CRC.CRC13.BBC.Factory.pas
@@ -0,0 +1,59 @@
+﻿/////////////////////////////////////////////////////////////////////////////////
+//*****************************************************************************//
+//* Project      : CryptoHash                                                 *//
+//* Latest Source: https://github.com/vampirsoft/CryptoHash                   *//
+//* Unit Name    : CryptoHash.inc                                             *//
+//* Author       : Сергей (LordVampir) Дворников                              *//
+//* Copyright 2021 LordVampir (https://github.com/vampirsoft)                 *//
+//* Licensed under MIT                                                        *//
+//*****************************************************************************//
+/////////////////////////////////////////////////////////////////////////////////
+
+unit chHash.CRC.CRC13.BBC.Factory;
+
+{$INCLUDE CryptoHash.inc}
+
+interface
+
+uses
+  chHash.CRC.CRC13,
+  chHash.CRC.CRC13.BBC;
+
+type
+
+{ TchCrc13BBC }
+
+  TchCrc13BBC = class sealed(chHash.CRC.CRC13.BBC.TchCrc13BBC)
+  private type
+    TInstance = {$IF DEFINED(SUPPORTS_INTERFACES)}IchCrc13{$ELSE}TchCrc13{$ENDIF};
+  private
+    class var FInstance: TInstance;
+  private
+    constructor Create; reintroduce;
+  public
+    class property Instance: TInstance read FInstance;
+  end;
+
+implementation
+
+{$IF NOT DEFINED(SUPPORTS_INTERFACES)}
+uses
+  System.SysUtils;
+{$ENDIF ~ NOT SUPPORTS_INTERFACES}
+
+{ TchCrc13BBC }
+
+constructor TchCrc13BBC.Create;
+begin
+  inherited Create;
+end;
+
+initialization
+  TchCrc13BBC.FInstance := TchCrc13BBC.Create;
+
+{$IF NOT DEFINED(SUPPORTS_INTERFACES)}
+finalization
+  FreeAndNil(TchCrc13BBC.FInstance);
+{$ENDIF ~ NOT SUPPORTS_INTERFACES}
+
+end.
